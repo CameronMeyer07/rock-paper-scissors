864a679cc4d2fcb1f969b2d41bd898c6d16cca74
Cameron <171266880+CameronMeyer07@users.noreply.github.com>
2024-06-19 18:28:11 +0200

    Create a function to receive computer input

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rock-paper-scissors
+Creating a new JavaScript game is a daunting task for someone 
+who is only learning how to use Javascript.
+
+The psuedocode is very simple and can be improved. It was the easiest way
+for me to divide the task into sub tasks.
+
+As I progress on my journey I will definitely learn to write better psuedocode.
--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+//Get input from computer
+
+function getComputerChoice(){
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice == 0){
+        console.log('rock');
+    } else if (computerChoice == 1){
+        console.log('paper');
+    } else {
+        console.log('scissor');
+    }
+}
+getComputerChoice()
+
+//Get input from player
+//Compare the results
+//Declare the winner 
+//Repeat the game for 5 rounds
+//After best of 5 rounds anounce overall winner
